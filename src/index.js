62a56efc021fa49d76ac3fba117507b0d3debf8a
qiaoyang <2282907674@qq.com>
2024-02-22 14:46:07 +0800

    refactor: update core function

--- src/index.js
+++ src/index.js
@@ -5,20 +5,20 @@ const htmlConditinalCompileIfdef = (htmlStr, targetPlat) => {
       return content
     }
     
-    // 删除当前被条件编译包裹的区块（包括条件编译注释指令）
+    // 未命中编译条件，删除当前被条件编译包裹的区块（包括条件编译注释指令）
     return ''
   })
 }
 
 const htmlConditinalCompileIfndef = (htmlStr, targetPlat) => {
   return htmlStr.replace(/<!-- ifndef targetPlat=(.*?) -->(.*?)<!-- endif -->/g, (match, platStr, content) => {
-    if (!platStr.split('|').includes(targetPlat)) {
-      // 命中编译条件：保留当前被条件编译包裹的区块（包括条件编译注释指令）
-      return content
+    if (platStr.split('|').includes(targetPlat)) {
+      // 命中编译条件：删除当前被条件编译包裹的区块（包括条件编译注释指令）
+      return ''
     }
     
-    // 删除当前被条件编译包裹的区块（包括条件编译注释指令）
-    return ''
+    // 未命中编译条件，保留当前被条件编译包裹的区块（包括条件编译注释指令）
+    return content
   })
 }
 
